b86273953ff1af7278ec63d1116a7e2809b97f96
jsangalang93 <jsangalang93@gmail.com>
2024-05-17 15:00:27 -0400

    forgot a comma

--- controllers/foods.js
+++ controllers/foods.js
@@ -2,7 +2,7 @@ const Food = require ('../models/food.js')
 const express = require('express')
 const router = express.Router()
 
-router.post('/' async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const createdFood = await Food.create(req.body)
         res.status(200).json(createdFood)
